c7cd8d921ec3cbe7cdef56a9c30e203661efac11
Muzammil803 <syedmuzammil691@gmail.com>
2022-07-20 09:27:49 +0500

    done

---
+++ app.js
@@ -0,0 +1,64 @@
+// function today(){
+//     document.write(new Date());
+// }
+// today();
+// function first(a,b){
+//     alert(a+b);
+// }
+// var a = prompt("Enter your first name");
+// var b = prompt("Enter your last name");
+// first(a,b);
+// function sum(a,b){
+//    document.write(a + b);
+// }
+// var a = +prompt("Enter a number");
+// var b = +prompt("Enter a number");
+// sum(a,b);
+// function calculation(a,b,c){
+//    if(c === "+"){
+//     document.write(a + b);
+//     }else if(c === "-"){
+//         document.write(a - b);
+//     }else if(c === "*"){
+//         document.write(a * b);
+//     }
+// }
+// var num1 = +prompt("Enter a number");
+// var num2 = +prompt("Enter a number");
+// var oper = +prompt("select a operater '+''-'''*");
+// calculation(a,b,c);
+// function square(a){
+//    document.write(a*a);
+
+// }
+// var a = +prompt("Enter a number");
+// square(a);
+// var a = +prompt("Enter starting number");
+// var b = +prompt("Enter ending number");
+// function loop(){
+//     for(i = a; i < b; ++i){
+//         document.write(i);
+//     }
+// }
+// loop();
+// var a = +prompt("Enter height of triangle");
+// var b = +prompt("Enter width of triangle");
+// function area(a,b){
+//     document.write(a * b);
+// }
+// area(a,b);
+// var a = "the quick brown fox";
+// var b = a.split(" ");
+// console.log(b);
+// var c = b.pop();
+// console.log(c)
+// function calc(a){
+//     document.write(2*3.14*a);
+// }
+// var a =+prompt("Enter the radius of circle");
+// calc(a);
+// function calc(a){
+//     document.write(3.14*(a*a));
+// }
+// var a =+prompt("Enter the radius of circle");
+//  calc(a);
